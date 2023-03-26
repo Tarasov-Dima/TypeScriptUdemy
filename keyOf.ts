35fcf2f3bf31b48ef15a0827777b4c11e4c19a03
ImDimaTarasov <dmytrotarasov9@gmail.com>
2023-03-26 21:25:48 +0200

    keyof TS

---
+++ keyOf.ts
@@ -0,0 +1,23 @@
+interface ICompany {
+	name: string;
+	debts: number;
+}
+
+type CompanyKeys = keyof ICompany;
+
+const keys: CompanyKeys = "debts";
+
+function printDebts<T, K extends keyof T, S extends keyof T>(
+	company: T,
+	name: K,
+	debts: S
+) {
+	console.log(`Company name: ${company[name]}, debts: ${company[debts]}`);
+}
+
+const hh: ICompany = {
+	name: "HH",
+	debts: 3213,
+};
+
+printDebts(hh, "name", "debts");
